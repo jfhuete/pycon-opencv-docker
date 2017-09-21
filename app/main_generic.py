ed3e7efb3732d99f5ceeac9322f969b6450e9a42
Juan Francisco Huete Verdejo <juanfrancisco.huete@a3sec.com>
2017-09-21 23:33:03 +0200

    Generic code modified with frame rate and WIDTH and HEIGHT properties

--- app/main_generic.py
+++ app/main_generic.py
@@ -1,7 +1,15 @@
 import cv2
+from time import sleep
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+
+WIDTH = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+HEIGHT = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+# frames per second
+
+FRAME_RATE = 25
 
 if __name__ == "__main__":
     while(True):
@@ -11,6 +19,7 @@ if __name__ == "__main__":
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+        sleep(1/FRAME_RATE)
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
