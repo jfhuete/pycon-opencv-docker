5d05b4fb60e7975a3f417b66dde6cadc27327ee4
Juan Francisco Huete Verdejo <juanfrancisco.huete@a3sec.com>
2017-09-23 16:58:42 +0200

    Cosmetic changes and remove numpy lib

--- app/main_generic.py
+++ app/main_generic.py
@@ -1,6 +1,5 @@
 import cv2
 from time import sleep
-import numpy as np
 
 
 if __name__ == "__main__":
@@ -18,6 +17,8 @@ if __name__ == "__main__":
 
     while(True):
         ret, frame = cap.read()
+
+        # Write your code here
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
